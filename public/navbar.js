1dd4f13988995a20a791942afa15767beaa94070
Sophie <sophie.gioff@gmail.com>
2022-10-01 18:07:42 -0400

    onclick to onClick

--- public/navbar.js
+++ public/navbar.js
@@ -1,29 +1,29 @@
 const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#" onclick="defaultModule()">BadBank</a>
+      <a className="navbar-brand" href="#" onClick="defaultModule()">BadBank</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item">
-            <a className="nav-link" href="#/CreateAccount/" onclick="loadCreateAccount()" id="createAccount">Create Account</a>
+            <a className="nav-link" href="#/CreateAccount/" onClick="loadCreateAccount()" id="createAccount">Create Account</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#/login/" onclick="loadLogin()" id="login">Login</a>
+            <a className="nav-link" href="#/login/" onClick="loadLogin()" id="login">Login</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#/deposit/" onclick="loadDeposit()" id="deposit">Deposit</a>
+            <a className="nav-link" href="#/deposit/" onClick="loadDeposit()" id="deposit">Deposit</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#/withdraw/" onclick="loadWithdraw()" id="withdraw">Withdraw</a>
+            <a className="nav-link" href="#/withdraw/" onClick="loadWithdraw()" id="withdraw">Withdraw</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#/balance/" onclick="loadBalance()" id="balance">Balance</a>
+            <a className="nav-link" href="#/balance/" onClick="loadBalance()" id="balance">Balance</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#/alldata/" onclick="loadAllData()" id="allData">AllData</a>
+            <a className="nav-link" href="#/alldata/" onClick="loadAllData()" id="allData">AllData</a>
           </li>
         </ul>
       </div>
